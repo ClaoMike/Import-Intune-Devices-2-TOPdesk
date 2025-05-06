a407e066181a614b3304c83abbcc2bafa5420530
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-05-06 11:07:52 +0200

    Small refactor

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import time
 
 # get the intune access token, it lasts for 3599 seconds, so we have to re-get it if an hour passes
 access_token = get_access_token()
+print(access_token)
 start_time = time.time()
 
 device_count=1
-current_page_of_devices_url = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
-while current_page_of_devices_url:
+next_devices_page_url = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
+while next_devices_page_url:
     # see if you need a new access token
     end_time = time.time()
     elapsed_seconds = end_time - start_time
@@ -19,17 +20,25 @@ while current_page_of_devices_url:
         start_time = time.time()
 
     # get devices from the current page and the link to the following page, if it exists
-    devices, current_page_of_devices_url = get_devices_from_curren_page(current_page_of_devices_url, access_token)
+    devices, next_devices_page_url = get_devices_from_curren_page(next_devices_page_url, access_token)
 
     for device in devices:
         print(f"{device_count}. ID: {device.get('id')} OS: {device.get('operatingSystem')}")
 
-        asset_id = create_device_asset(
-            get_device_type(
-                device.get('operatingSystem')
-            ), device)
+        # if device exists as an asset:
+        #     if asset is not equal to device
+        #         update device
+        #     else
+        #         continue
+        # else
+        #     create asset with this device
 
-        # if the person card is found, attach the asset to it
-        assign_user_to_asset(dvc=device, topdesk_asset_id=asset_id)
+        # asset_id = create_device_asset(
+        #     get_device_type(
+        #         device.get('operatingSystem')
+        #     ), device)
+        #
+        # # if the person card is found, attach the asset to it
+        # assign_user_to_asset(dvc=device, topdesk_asset_id=asset_id)
 
         device_count += 1
--- utils.py
+++ utils.py
@@ -21,34 +21,6 @@ class DeviceType(Enum):
     COMPUTER = "COMPUTER"
     MOBILE = "MOBILE"
 
-def evaluateResponse(response: Response):
-    """
-    Categorizes and handles the status code of an HTTP response.
-
-    Args:
-        response (requests.Response): The HTTP response object to evaluate.
-
-    Raises:
-        RequestFailedException: If the status code is not in the 2xx range.
-    """
-
-    if 200 <= response.status_code < 300:
-        return
-    elif response.status_code == 400:
-        try:
-            response_text_as_json = json.loads(response.text)
-            error_text = response_text_as_json.get('errors')[0]
-
-            if error_text.get('fieldName') == 'name' and error_text.get('fieldTitle') == 'Asset ID' and error_text.get('message') == 'This ID is already in use.':
-                print("Error: Asset ID is already in use.")
-                return
-        except:
-            error_message = f"Error {response.status_code}: {response.text}"
-            raise ValueError(error_message)
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
-
 def make_request(request_type: RequestType, url: str, headers, data=None, auth=None, json=None, params=None):
     """
         Makes an HTTP request with the specified request type.
@@ -92,18 +64,10 @@ def make_request(request_type: RequestType, url: str, headers, data=None, auth=N
 
     elif request_type == RequestType.PUT:
         response = requests.put(**request_params)
-
     else:
         raise ValueError("Invalid request type")
 
-    # Evaluate the response (this may log errors and raise exceptions if necessary)
-    evaluateResponse(response)
-
-    # Return the parsed JSON response
-    try:
-        return response.json()
-    except:
-        return None
+    return response
 
 def get_access_token():
     response = make_request(
@@ -120,7 +84,12 @@ def get_access_token():
         }
     )
 
-    return response['access_token']
+    if 200 <= response.status_code < 300:
+        print("Successfully obtained access token")
+        return response.json()['access_token']
+    else:
+        error_message = f"Error {response.status_code}: {response.text}"
+        raise ValueError(error_message)
 
 def get_devices_from_curren_page(url, tkn):
     response = make_request(
@@ -131,142 +100,164 @@ def get_devices_from_curren_page(url, tkn):
             'Content-Type': 'application/json'
         },
     )
-    return response.get('value'), response.get('@odata.nextLink')
 
-def get_topdesk_user_id_by_mainframe(user_id):
-    topdesk_person = make_request(
-        request_type=RequestType.GET,
-        url=f"https://dlfseeds.topdesk.net/tas/api/persons?query=mainframeLoginName=={user_id}",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        }
-    )
-
-    try:
-        if topdesk_person[0].get('status') != 'personArchived':
-            return topdesk_person[0].get('id')
-    except (AttributeError, KeyError, IndexError, TypeError):
-        return None
-
-
-def assign_user_to_asset(dvc, topdesk_asset_id):
-    if topdesk_asset_id is None:
-        print("Asset ID is required to assign user to asset!")
-        return
-    # get the ID of the user that uses the device;
-    # This is the same as a persons' mainframe ID, stored in TOPdesk person cards
-    user_id = dvc.get("userId")
-
-    if user_id != "":
-        print(f"User ID: {user_id}")
-
-        topdesk_person_id = get_topdesk_user_id_by_mainframe(user_id)  # get the TOPdesk person card, searching by the above mainframe
-
-        if topdesk_person_id is not None:
-            print("Found its TOPdesk Person card.")
-
-            # if the person card is found, attach the asset to it
-            response = make_request(
-                request_type=RequestType.PUT,
-                url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{topdesk_asset_id}/assignments",
-                auth=(topdesk_username, topdesk_password),
-                headers={
-                    'Content-Type': 'application/json'
-                },
-                json={
-                    "linkToId": topdesk_person_id,
-                    "linkType": "person"
-                }
-            )
-
-            return response
-        else:
-            print(f"User ID exists on Azure, but there is no Person card for it in TOPdesk.")
+    if 200 <= response.status_code < 300:
+        return response.json().get('value'), response.json().get('@odata.nextLink')
     else:
-        print("No user assigned to this device!")
-        return
+        error_message = f"Error {response.status_code}: {response.text}"
+        raise ValueError(error_message)
 
-def get_device_type(operating_system: str):
-    if operating_system in skip_os:  # Skip these
-        return None
-    elif operating_system in computer_os:
-        return DeviceType.COMPUTER
-    elif operating_system in mobile_os:
-        return DeviceType.MOBILE
-    else:
-        print("New OS detected - please take action")
-        return None
 
-def validate_topdesk_asset(asset):
-    if asset is None:  # no device created, thus we move to the next one
-        return False
+# def get_topdesk_user_id_by_mainframe(user_id):
+#     topdesk_person = make_request(
+#         request_type=RequestType.GET,
+#         url=f"https://dlfseeds.topdesk.net/tas/api/persons?query=mainframeLoginName=={user_id}",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         }
+#     )
+#
+#     try:
+#         if topdesk_person[0].get('status') != 'personArchived':
+#             return topdesk_person[0].get('id')
+#     except (AttributeError, KeyError, IndexError, TypeError):
+#         return None
 
-    if asset.get('data') is None:  # if no data here, it means it must be an accepted error
-        print("No data for this device, check for errors!")
-        return False
 
-    return True
+# def assign_user_to_asset(dvc, topdesk_asset_id):
+#     if topdesk_asset_id is None:
+#         print("Asset ID is required to assign user to asset!")
+#         return
+#     # get the ID of the user that uses the device;
+#     # This is the same as a persons' mainframe ID, stored in TOPdesk person cards
+#     user_id = dvc.get("userId")
+#
+#     if user_id != "":
+#         print(f"User ID: {user_id}")
+#
+#         topdesk_person_id = get_topdesk_user_id_by_mainframe(user_id)  # get the TOPdesk person card, searching by the above mainframe
+#
+#         if topdesk_person_id is not None:
+#             print("Found its TOPdesk Person card.")
+#
+#             # if the person card is found, attach the asset to it
+#             response = make_request(
+#                 request_type=RequestType.PUT,
+#                 url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{topdesk_asset_id}/assignments",
+#                 auth=(topdesk_username, topdesk_password),
+#                 headers={
+#                     'Content-Type': 'application/json'
+#                 },
+#                 json={
+#                     "linkToId": topdesk_person_id,
+#                     "linkType": "person"
+#                 }
+#             )
+#
+#             return response
+#         else:
+#             print(f"User ID exists on Azure, but there is no Person card for it in TOPdesk.")
+#     else:
+#         print("No user assigned to this device!")
+#         return
 
-def create_device_asset(device_type: DeviceType, dvc):
-    if device_type is None:
-        print("Unknown OS")
-        return None
+# def get_device_type(operating_system: str):
+#     if operating_system in skip_os:  # Skip these
+#         return None
+#     elif operating_system in computer_os:
+#         return DeviceType.COMPUTER
+#     elif operating_system in mobile_os:
+#         return DeviceType.MOBILE
+#     else:
+#         print("New OS detected - please take action")
+#         return None
 
-    if device_type == DeviceType.COMPUTER:
-        category_key = topdesk_computer_category_id
-        name = dvc.get('serialNumber')
+# def validate_topdesk_asset(asset):
+#     if asset is None:  # no device created, thus we move to the next one
+#         return False
+#
+#     if asset.get('data') is None:  # if no data here, it means it must be an accepted error
+#         print("No data for this device, check for errors!")
+#         return False
+#
+#     return True
 
-    elif device_type == DeviceType.MOBILE:
-        category_key = topdesk_mobile_category_id
-        name = dvc.get('id')
+# def create_device_asset(device_type: DeviceType, dvc):
+#     if device_type is None:
+#         print("Unknown OS")
+#         return None
+#
+#     if device_type == DeviceType.COMPUTER:
+#         category_key = topdesk_computer_category_id
+#         name = dvc.get('serialNumber')
+#
+#     elif device_type == DeviceType.MOBILE:
+#         category_key = topdesk_mobile_category_id
+#         name = dvc.get('id')
+#
+#     else:
+#         print("Unknown device type")
+#         return None
+#
+#     print(f"TOPdesk name: {device_type.value}-{name}")
+#
+#     response = make_request(
+#         request_type=RequestType.POST,
+#         url="https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         },
+#         json={
+#             "name": f"{device_type.value}-{name}", # asset id
+#             "type_id": category_key, # asset template
+#
+#             "intune-id": dvc.get("id"), # Intune ID
+#             "azure-ad-registered": dvc.get("azureADRegistered"),
+#             "azure-id": dvc.get("azureADDeviceId"), # azure ID
+#             "serial-number": dvc.get("serialNumber"),
+#             "name-1": dvc.get("deviceName"),
+#             "manufacturer-1": dvc.get("manufacturer"),
+#             "model-1": dvc.get("model"),
+#             "operating-system": dvc.get('operatingSystem'),
+#             "os-version": dvc.get("osVersion"),
+#             "enrollment-date": dvc.get("enrolledDateTime"),
+#             "last-check-in": dvc.get("lastSyncDateTime"),
+#             "management-certificate-expiration-date": dvc.get("managementCertificateExpirationDate"),
+#             "ismanaged": dvc.get("isSupervised"),
+#             "imei": dvc.get("imei"),
+#             "encrypted": dvc.get("isEncrypted"),
+#             "subscriber-carrier": dvc.get("subscriberCarrier"),
+#             "total-storage": dvc.get("totalStorageSpaceInBytes"),
+#             "storage": dvc.get("freeStorageSpaceInBytes"),
+#             "compliance-status": dvc.get("complianceState"),
+#             "ownership": dvc.get("managedDeviceOwnerType"),
+#         }
+#     )
+#
+#     print(response)
+#
+#     if validate_topdesk_asset(response):
+#         topdesk_id = response.get('data').get('unid')
+#         print(f"TOPdesk ID: {topdesk_id}")
+#
+#         return topdesk_id  # extract the newly created asset's ID
+#     else:
+#         return None
 
-    else:
-        print("Unknown device type")
-        return None
-
-    print(f"TOPdesk name: {device_type.value}-{name}")
-
-    response = make_request(
-        request_type=RequestType.POST,
-        url="https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        },
-        json={
-            "name": f"{device_type.value}-{name}", # asset id
-            "type_id": category_key, # asset template
-
-            "intune-id": dvc.get("id"), # Intune ID
-            "azure-ad-registered": dvc.get("azureADRegistered"),
-            "azure-id": dvc.get("azureADDeviceId"), # azure ID
-            "serial-number": dvc.get("serialNumber"),
-            "name-1": dvc.get("deviceName"),
-            "manufacturer-1": dvc.get("manufacturer"),
-            "model-1": dvc.get("model"),
-            "operating-system": dvc.get('operatingSystem'),
-            "os-version": dvc.get("osVersion"),
-            "enrollment-date": dvc.get("enrolledDateTime"),
-            "last-check-in": dvc.get("lastSyncDateTime"),
-            "management-certificate-expiration-date": dvc.get("managementCertificateExpirationDate"),
-            "ismanaged": dvc.get("isSupervised"),
-            "imei": dvc.get("imei"),
-            "encrypted": dvc.get("isEncrypted"),
-            "subscriber-carrier": dvc.get("subscriberCarrier"),
-            "total-storage": dvc.get("totalStorageSpaceInBytes"),
-            "storage": dvc.get("freeStorageSpaceInBytes"),
-            "compliance-status": dvc.get("complianceState"),
-            "ownership": dvc.get("managedDeviceOwnerType"),
-        }
-    )
-
-    print(response)
-
-    if validate_topdesk_asset(response):
-        topdesk_id = response.get('data').get('unid')
-        print(f"TOPdesk ID: {topdesk_id}")
-
-        return topdesk_id  # extract the newly created asset's ID
-    else:
-        return None
+# def search_for_topdesk_asset_by_asset_name(asset_name):
+#     topdesk_person = make_request(
+#         request_type=RequestType.GET,
+#         url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets?nameFragment={asset_name}",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         }
+#     )
+#
+#     try:
+#         if topdesk_person[0].get('status') != 'personArchived':
+#             return topdesk_person[0].get('id')
+#     except (AttributeError, KeyError, IndexError, TypeError):
+#         return None
