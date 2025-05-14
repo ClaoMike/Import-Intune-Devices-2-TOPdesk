af11262a5aea282787684b7ebb03880af271da05
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-05-14 10:36:15 +0200

    Getting all TOPdesk assets

--- device_methods.py
+++ device_methods.py
@@ -1,28 +1,15 @@
-# import json
-from operating_systems import *
-# from topdesk_methods import *
-from device_types import *
-
-def generate_asset_name(platform, device):
-    if platform == "intune":
-        id = device.get('azureADDeviceId')
-    else:
-        id = device.get('id')
-
-    operating_system = device.get('operatingSystem')
-    type = get_device_type(operating_system).value
-
-    return f"{type}-{id}"
+from topdesk_methods import *
+from Device import *
 
 def get_device_template(device):
     operating_system = device.get('operatingSystem')
-    type = get_device_type(operating_system)
+    type = Device.get_device_type(operating_system)
 
-    if type is DeviceType.DEVICE:  # Skip these
+    if type is Device.Type.DEVICE:  # Skip these
         return topdesk_device_category_id
-    elif type is DeviceType.COMPUTER:
+    elif type is Device.Type.COMPUTER:
         return topdesk_computer_category_id
-    elif type is DeviceType.MOBILE:
+    elif type is Device.Type.MOBILE:
         return topdesk_mobile_category_id
     else:
         print("New OS detected - please take action")
