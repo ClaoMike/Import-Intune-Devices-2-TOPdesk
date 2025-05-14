a07b8c68f38297c56f360f2d465d5495496d2acd
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-05-14 10:49:28 +0200

    Getting assets and devices in parallel

--- topdesk_methods.py
+++ topdesk_methods.py
@@ -1,133 +1,177 @@
 import requests
 from env_variables import *
 
-def assign_user_to_asset(person_card_id, asset_id):
-    response = requests.put(
-        url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}/assignments",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        },
-        json={
-            "linkToId": person_card_id,
-            "linkType": "person"
+def fetch_all_assets(template_id, page_size=1000):
+    all_assets = []
+    page_start = 0
+    has_more = True
+
+    fields = [
+        "name", "intune-id", "azure-ad-registered", "azure-id", "serial-number",
+        "name-1", "manufacturer-1", "model-1", "operating-system", "os-version",
+        "enrollment-date", "last-check-in", "management-certificate-expiration-date",
+        "ismanaged", "imei", "encrypted", "subscriber-carrier", "total-storage",
+        "storage", "compliance-status", "ownership", "user-id"
+    ]
+
+    base_url = "https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets"
+    auth = (topdesk_username, topdesk_password)
+    headers = {
+        "Accept": "application/x.topdesk-am-assets-v2+json",
+        "Content-Type": "application/json"
+    }
+
+    fields_param = ",".join(fields)
+
+    while has_more:
+        params = {
+            "templateId": template_id,
+            "fields": fields_param,
+            "pageStart": page_start,
+            "pageSize": page_size
         }
-    )
 
-    if 200 <= response.status_code < 300:
-        print("Successfully assigned user to asset")
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
+        response = requests.get(base_url, headers=headers, params=params, auth=auth)
+        if response.status_code not in [200, 206]:
+            raise Exception(f"Error {response.status_code}: {response.text}")
 
-def get_topdesk_user_id_by_mainframe(user_id):
-    response = requests.get(
-        url=f"https://dlfseeds.topdesk.net/tas/api/persons?query=mainframeLoginName=={user_id}",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        }
-    )
+        data = response.json()
+        assets = data.get("dataSet")
+        all_assets.extend(assets)
+        print(f"[{template_id}] Fetched {len(assets)} assets (total so far: {len(all_assets)})")
 
-    if 200 <= response.status_code < 300:
-        if response.text != '':
-            # print(response.json())
+        has_more = response.status_code == 206
+        page_start += page_size
 
-            if len(response.json()) == 0:
-                return None
+    return all_assets
 
-            person_card = response.json()[0]
-            if person_card.get('status') != 'personArchived':
-                return person_card.get('id')
-            else:
-                return None
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
-
-def search_for_topdesk_asset_by_asset_name(asset_name):
-    response = requests.get(
-        url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets?nameFragment={asset_name}",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        }
-    )
-
-    if 200 <= response.status_code < 300:
-        data_set = response.json().get('dataSet')
-        if len(data_set) == 0:
-            return None
-        else:
-            return data_set[0].get('id')
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
-
-
-def get_asset_data(asset_id):
-    response = requests.get(
-        url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        },
-    )
-
-    if 200 <= response.status_code < 300:
-        data = response.json().get('data')
-        # print(f"Successfully retrieve the asset's data: {data}")
-
-        return data
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
-
-def get_asset_assignment_link(asset_id):
-    response = requests.get(
-        url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}/assignments",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        },
-    )
-
-    if 200 <= response.status_code < 300:
-        try:
-            return response.json().get('persons')[0].get('linkId')
-        except:
-            return None
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
-
-def remove_asset_assignment_person(asset_id, link_id):
-    response = requests.delete(
-        url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}/assignments/{link_id}",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        },
-    )
-
-    if 200 <= response.status_code < 300:
-        print("Successfully deleted the assignment link!")
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
-
-def update_asset_data(asset_id, data):
-    response = requests.post(
-        url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        },
-        json=data
-    )
-
-    if 200 <= response.status_code < 300:
-        print(f"Successfully updated the asset with ID:{asset_id}")
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
+# def assign_user_to_asset(person_card_id, asset_id):
+#     response = requests.put(
+#         url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}/assignments",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         },
+#         json={
+#             "linkToId": person_card_id,
+#             "linkType": "person"
+#         }
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         print("Successfully assigned user to asset")
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
+#
+# def get_topdesk_user_id_by_mainframe(user_id):
+#     response = requests.get(
+#         url=f"https://dlfseeds.topdesk.net/tas/api/persons?query=mainframeLoginName=={user_id}",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         }
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         if response.text != '':
+#             # print(response.json())
+#
+#             if len(response.json()) == 0:
+#                 return None
+#
+#             person_card = response.json()[0]
+#             if person_card.get('status') != 'personArchived':
+#                 return person_card.get('id')
+#             else:
+#                 return None
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
+#
+# def search_for_topdesk_asset_by_asset_name(asset_name):
+#     response = requests.get(
+#         url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets?nameFragment={asset_name}",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         }
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         data_set = response.json().get('dataSet')
+#         if len(data_set) == 0:
+#             return None
+#         else:
+#             return data_set[0].get('id')
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
+#
+#
+# def get_asset_data(asset_id):
+#     response = requests.get(
+#         url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         },
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         data = response.json().get('data')
+#         # print(f"Successfully retrieve the asset's data: {data}")
+#
+#         return data
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
+#
+# def get_asset_assignment_link(asset_id):
+#     response = requests.get(
+#         url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}/assignments",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         },
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         try:
+#             return response.json().get('persons')[0].get('linkId')
+#         except:
+#             return None
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
+#
+# def remove_asset_assignment_person(asset_id, link_id):
+#     response = requests.delete(
+#         url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}/assignments/{link_id}",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         },
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         print("Successfully deleted the assignment link!")
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
+#
+# def update_asset_data(asset_id, data):
+#     response = requests.post(
+#         url=f"https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets/{asset_id}",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         },
+#         json=data
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         print(f"Successfully updated the asset with ID:{asset_id}")
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
--- utils.py
+++ utils.py
@@ -5,122 +5,156 @@ from microsoft_methods import *
 from device_methods import *
 from comparisons import *
 
-def create_asset_for(platform, device, tkn):
-    # create_asset
-    asset_name = generate_asset_name(platform, device)
-    template_id = get_device_template(device)
+def fetch_devices_and_assets_in_parallel():
+    all_assets = []
+    topdesk_categories = [
+        topdesk_computer_category_id,
+        topdesk_mobile_category_id,
+        topdesk_device_category_id
+    ]
 
-    if platform == "intune":
-        userId = device.get('userId')
-        json = generate_intune_asset_as_json(device, asset_name, template_id)
-    else:
-        userId = get_user_id_of_azure_device(device, tkn)
-        json = generate_azure_asset_as_json(device, asset_name, template_id, userId)
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = []
 
-    response = requests.post(
-        url="https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets",
-        auth=(topdesk_username, topdesk_password),
-        headers={
-            'Content-Type': 'application/json'
-        },
-        json=json
-    )
+        # Start asset fetching for each category
+        for category_id in topdesk_categories:
+            futures.append(executor.submit(fetch_all_assets, category_id))
 
-    if 200 <= response.status_code < 300:
-        asset_id = response.json().get('data').get('unid')
-        print(f"Successfully created the asset with ID:{asset_id}")
-    else:
-        error_message = f"Error {response.status_code}: {response.text}"
-        raise ValueError(error_message)
+        # Start fetching Azure and Intune devices
+        device_future = executor.submit(fetch_devices)
 
-    # assign_user
-    if userId is None or userId == '':
-        return
+        # Wait for asset futures
+        for future in as_completed(futures):
+            try:
+                assets = future.result()
+                all_assets.extend(assets)
+            except Exception as e:
+                print(f"[✗] Error fetching assets: {e}")
 
-    topdesk_person_card_id = get_topdesk_user_id_by_mainframe(userId)
+        # Wait for devices
+        devices = device_future.result()
 
-    if topdesk_person_card_id is None:
-        return
+    print(f"\n[✓] Total assets fetched: {len(all_assets)}")
+    print(f"[✓] Total devices fetched: {len(devices)}")
 
-    print(f"TOPdesk card ID: {topdesk_person_card_id}")
+    return devices, all_assets
 
-    assign_user_to_asset(topdesk_person_card_id, asset_id)
-
-def update_asset(device, asset_id, platform, tkn):
-    asset_data = get_asset_data(asset_id)
-    asset_name = generate_asset_name(platform, device)
-    template_id = get_device_template(device)
-
-    is_asset_up_to_date = True
-    is_user_the_same = True
-
-    if platform == "intune":
-        is_asset_up_to_date = compare_device_with_intune(device, asset_data)
-        # print(f"Equal? {is_asset_up_to_date}")
-        # print()
-        # print(f"{asset_data.get('name')} vs. {asset_name}")
-        # print(f"{asset_data.get('user-id')} vs. {device.get('userId')}")
-
-        if asset_data.get("user-id") != device.get("userId"):
-            # print("Not the same user!")
-            # print(f"{asset_data.get('user-id')} vs. {device.get('userId')}")
-            is_user_the_same = False
-
-        if not is_asset_up_to_date:
-            updated_asset = generate_intune_asset_as_json(device, asset_name, template_id)
-            # print(updated_asset)
-            update_asset_data(asset_id, updated_asset)
-
-        if not is_user_the_same:
-            # print("----------------------------------------------")
-            # print(asset_data)
-            # print(device)
-            # get current link
-            link_id = get_asset_assignment_link(asset_id)
-
-            if link_id is not None:
-                # remove link
-                remove_asset_assignment_person(asset_id, link_id)
-                # print(f"Removing the link: {link_id}")
-
-            # update asset with new user id (eventually empty)
-            new_user_data = generate_new_user_asset_data_as_json(asset_name, template_id, device.get("userId"))
-            # print(new_user_data)
-            update_asset_data(asset_id, new_user_data)
-
-            if device.get("userId") != '':
-                # print("Adding the link")
-                person_card_id = get_topdesk_user_id_by_mainframe(device.get("userId"))
-
-                # link new user
-                assign_user_to_asset(person_card_id, asset_id)
-            # print("===========================================")
-    else:
-        is_asset_up_to_date = compare_device_with_azure(device, asset_data)
-        user_Id = get_user_id_of_azure_device(device, tkn)
-
-        if asset_data.get("user-id") != user_Id:
-            is_user_the_same = False
-
-        if not is_asset_up_to_date:
-            updated_asset = generate_azure_asset_as_json(device, asset_name, template_id, user_Id)
-            update_asset_data(asset_id, updated_asset)
-
-        if not is_user_the_same:
-            link_id = get_asset_assignment_link(asset_id)
-
-            if link_id is not None:
-                # remove link
-                remove_asset_assignment_person(asset_id, link_id)
-
-            # update asset with new user id (eventually empty)
-            new_user_data = generate_new_user_asset_data_as_json(asset_name, template_id, device.get("userId"))
-            update_asset_data(asset_id, new_user_data)
-
-            if user_Id != '':
-                person_card_id = get_topdesk_user_id_by_mainframe(user_Id)
-
-                print(person_card_id)
-
-                # link new user
-                assign_user_to_asset(person_card_id, asset_id)
+# def create_asset_for(platform, device, tkn):
+#     # create_asset
+#     asset_name = generate_asset_name(platform, device)
+#     template_id = get_device_template(device)
+#
+#     if platform == "intune":
+#         userId = device.get('userId')
+#         json = generate_intune_asset_as_json(device, asset_name, template_id)
+#     else:
+#         userId = get_user_id_of_azure_device(device, tkn)
+#         json = generate_azure_asset_as_json(device, asset_name, template_id, userId)
+#
+#     response = requests.post(
+#         url="https://dlfseeds.topdesk.net/tas/api/assetmgmt/assets",
+#         auth=(topdesk_username, topdesk_password),
+#         headers={
+#             'Content-Type': 'application/json'
+#         },
+#         json=json
+#     )
+#
+#     if 200 <= response.status_code < 300:
+#         asset_id = response.json().get('data').get('unid')
+#         print(f"Successfully created the asset with ID:{asset_id}")
+#     else:
+#         error_message = f"Error {response.status_code}: {response.text}"
+#         raise ValueError(error_message)
+#
+#     # assign_user
+#     if userId is None or userId == '':
+#         return
+#
+#     topdesk_person_card_id = get_topdesk_user_id_by_mainframe(userId)
+#
+#     if topdesk_person_card_id is None:
+#         return
+#
+#     print(f"TOPdesk card ID: {topdesk_person_card_id}")
+#
+#     assign_user_to_asset(topdesk_person_card_id, asset_id)
+#
+# def update_asset(device, asset_id, platform, tkn):
+#     asset_data = get_asset_data(asset_id)
+#     asset_name = generate_asset_name(platform, device)
+#     template_id = get_device_template(device)
+#
+#     is_asset_up_to_date = True
+#     is_user_the_same = True
+#
+#     if platform == "intune":
+#         is_asset_up_to_date = compare_device_with_intune(device, asset_data)
+#         # print(f"Equal? {is_asset_up_to_date}")
+#         # print()
+#         # print(f"{asset_data.get('name')} vs. {asset_name}")
+#         # print(f"{asset_data.get('user-id')} vs. {device.get('userId')}")
+#
+#         if asset_data.get("user-id") != device.get("userId"):
+#             # print("Not the same user!")
+#             # print(f"{asset_data.get('user-id')} vs. {device.get('userId')}")
+#             is_user_the_same = False
+#
+#         if not is_asset_up_to_date:
+#             updated_asset = generate_intune_asset_as_json(device, asset_name, template_id)
+#             # print(updated_asset)
+#             update_asset_data(asset_id, updated_asset)
+#
+#         if not is_user_the_same:
+#             # print("----------------------------------------------")
+#             # print(asset_data)
+#             # print(device)
+#             # get current link
+#             link_id = get_asset_assignment_link(asset_id)
+#
+#             if link_id is not None:
+#                 # remove link
+#                 remove_asset_assignment_person(asset_id, link_id)
+#                 # print(f"Removing the link: {link_id}")
+#
+#             # update asset with new user id (eventually empty)
+#             new_user_data = generate_new_user_asset_data_as_json(asset_name, template_id, device.get("userId"))
+#             # print(new_user_data)
+#             update_asset_data(asset_id, new_user_data)
+#
+#             if device.get("userId") != '':
+#                 # print("Adding the link")
+#                 person_card_id = get_topdesk_user_id_by_mainframe(device.get("userId"))
+#
+#                 # link new user
+#                 assign_user_to_asset(person_card_id, asset_id)
+#             # print("===========================================")
+#     else:
+#         is_asset_up_to_date = compare_device_with_azure(device, asset_data)
+#         user_Id = get_user_id_of_azure_device(device, tkn)
+#
+#         if asset_data.get("user-id") != user_Id:
+#             is_user_the_same = False
+#
+#         if not is_asset_up_to_date:
+#             updated_asset = generate_azure_asset_as_json(device, asset_name, template_id, user_Id)
+#             update_asset_data(asset_id, updated_asset)
+#
+#         if not is_user_the_same:
+#             link_id = get_asset_assignment_link(asset_id)
+#
+#             if link_id is not None:
+#                 # remove link
+#                 remove_asset_assignment_person(asset_id, link_id)
+#
+#             # update asset with new user id (eventually empty)
+#             new_user_data = generate_new_user_asset_data_as_json(asset_name, template_id, device.get("userId"))
+#             update_asset_data(asset_id, new_user_data)
+#
+#             if user_Id != '':
+#                 person_card_id = get_topdesk_user_id_by_mainframe(user_Id)
+#
+#                 print(person_card_id)
+#
+#                 # link new user
+#                 assign_user_to_asset(person_card_id, asset_id)
