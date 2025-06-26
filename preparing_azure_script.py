a24fd8c2b4c885e60002e0bdc82904a50d597ab7
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-06-26 12:58:45 +0200

    Saving only those that have an Azure ID

--- preparing_azure_script.py
+++ preparing_azure_script.py
@@ -886,10 +886,49 @@ def get_microsoft_defender_devices():
     )
 
     if 200 <= response.status_code < 300:
-        return response.json().get('value')
+        microsoft_defender_devices = response.json().get('value')
+        microsoft_defender_devices_with_azure_id = [device for device in microsoft_defender_devices if device.get("aadDeviceId") is not None]
+
+        return microsoft_defender_devices_with_azure_id
     else:
         error_message = f"Error {response.status_code}: {response.text}"
         raise ValueError(error_message)
 
-for device in get_microsoft_defender_devices():
+microsoft_defender_devices = get_microsoft_defender_devices()
+for device in microsoft_defender_devices:
     print(device)
+
+print(len(microsoft_defender_devices))
+
+# class AzureDevice:
+#     def __init__(self, data: dict):
+#
+#         # extract relevant data
+#         self.id = data.get("id") # this is the Azure ID
+#
+#         self.approximate_last_sign_in: Optional[datetime] = (
+#             datetime.strptime(
+#                 data.get("approximateLastSignInDateTime"),
+#                 "%Y-%m-%dT%H:%M:%SZ") if data.get("approximateLastSignInDateTime") else None
+#         )
+#         self.display_name: Optional[str] = data.get("displayName")
+#         self.is_managed: Optional[bool] = bool(data.get("isManaged"))
+#         self.manufacturer: Optional[str] = data.get("manufacturer")
+#         self.model: Optional[str] = data.get("model")
+#         self.operating_system: Optional[str] = data.get("operatingSystem")
+#         self.operating_system_version: Optional[str] = data.get("operatingSystemVersion")
+#         self.registration_date_time: Optional[datetime] = (
+#             datetime.strptime(
+#                 data.get("registrationDateTime"),
+#                 "%Y-%m-%dT%H:%M:%SZ") if data.get("registrationDateTime") else None
+#         )
+#
+#         self.device_type: Optional[Device.Type] = Device.get_device_type(
+#             operating_system=self.operating_system
+#         )
+#
+#         # compute the topdesk asset name
+#         self.topdesk_asset_name = Device.compute_topdesk_asset_name(
+#             os=self.operating_system,
+#             device_id=self.device_id
+#         )
